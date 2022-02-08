6ad2cfd18a27dd4c41e99c14a509d3ce58d71e34
qkrxodud00 <qkrxodud00@gmail.com>
2022-02-08 22:24:18 +0900

    *메인페이지  - 테마설정

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'input.dart';
+
+void main() {
+  runApp(const BMICalc());
+}
+
+class BMICalc extends StatelessWidget {
+  const BMICalc({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith( // copyWith를 사용해서 다크 테마에서 내가 원하는 테마로 변경 가능하다.
+          scaffoldBackgroundColor: Color(0xff0a0e21),//App의 scaffold의 백그라운드 색상이 변경된다.
+          textTheme: const TextTheme(
+            bodyText2: TextStyle(color: Colors.white), // 기본 글씨 색상 변경
+          )
+      ),
+      home: const Input(),
+    );
+  }
+}
+
+
