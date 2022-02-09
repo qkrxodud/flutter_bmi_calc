f7044aae371827cc032d8f35fa53a9cada304091
qkrxodud00 <qkrxodud00@gmail.com>
2022-02-09 23:25:53 +0900

    *라우터 수정  - 클래스 명칭으로 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bmi_calc/result.dart';
 
 import 'input.dart';
 
@@ -12,9 +13,9 @@ class BMICalc extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      initialRoute: '/input',
       routes: {
-        '/' : (context) => const Input(),
+        '/input' : (context) => const Input(),
       },
       theme: ThemeData.dark().copyWith( // copyWith를 사용해서 다크 테마에서 내가 원하는 테마로 변경 가능하다.
           scaffoldBackgroundColor: Color(0xff0a0e21),//App의 scaffold의 백그라운드 색상이 변경된다.
