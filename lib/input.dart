f1bd714808f15bb18e1d33f4d788d0ba8327c13a
qkrxodud00 <qkrxodud00@gmail.com>
2022-02-09 17:18:21 +0900

    *인풋페이지 화면 추가 작업

--- lib/input.dart
+++ lib/input.dart
@@ -13,7 +13,7 @@ class Input extends StatefulWidget {
 }
 
 class _InputState extends State<Input> {
-  PersonData personData = PersonData(Gender.male, 100, 100, 100);
+  PersonData personData = PersonData(gender: Gender.male, height: 130.0, weight: 100, age: 20);
 
   @override
   Widget build(BuildContext context) {
@@ -30,43 +30,166 @@ class _InputState extends State<Input> {
               flex: 1,
               child: Row(
                 children: [
-                  Expanded(
-                    flex: 1,
-                      child: genderCard(Gender.male)),
-                  const SizedBox(width: 5,),
-                  Expanded(
-                      flex: 1,
-                      child: genderCard(Gender.female)),
+                  Expanded(flex: 1, child: genderCard(Gender.male)),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(flex: 1, child: genderCard(Gender.female)),
                 ],
               ),
             ),
-            const SizedBox(height: 5,),
+            const SizedBox(
+              height: 5,
+            ),
             Expanded(
               flex: 1,
               child: Container(
-                decoration: BoxDecoration(
-                    color: const Color(0xff1d1e33),
-                  borderRadius: BorderRadius.circular(10.0)
+                width: double.infinity,
+                decoration: BoxDecoration(color: const Color(0xff1d1e33), borderRadius: BorderRadius.circular(10.0)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('HEIGHT'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic, //로우에서 글자를 밑에 기준으로 잡아준다.
+                      children: [
+                        Text(personData.getHeight().toStringAsFixed(2)),
+                        const Text('cm'),
+                      ],
+                    ),
+                    Slider(
+                        value: personData.getHeight(),
+                        min: 120,
+                        max: 220,
+                        onChanged: (double newValue) {
+                          setState(() {
+                            personData.setHeight(newValue);
+                          });
+                        })
+                  ],
                 ),
                 height: 50,
               ),
             ),
-            const SizedBox(height: 5,),
+            const SizedBox(
+              height: 5,
+            ),
             Expanded(
               flex: 1,
               child: Row(
                 children: [
                   Expanded(
                       flex: 1,
-                      child: card(nonActiveCardColor)),
-                  const SizedBox(width: 5,),
+                      child: card(
+                          nonActiveCardColor,
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text('WEIGHT'),
+                              Text('${personData.getWeight()}'),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  RawMaterialButton(
+                                      elevation: 6.0,
+                                      fillColor: Color(0xff4c4f5e),
+                                      constraints: const BoxConstraints.tightFor(
+                                        height: 40.0,
+                                        width: 40.0,
+                                      ),
+                                      child: const Text(
+                                        '+',
+                                        style: TextStyle(),
+                                      ),
+                                      shape: CircleBorder(),
+                                      onPressed: () {
+                                        setState(() {
+                                          personData.addWeight();
+                                        });
+                                      }),
+                                  RawMaterialButton(
+                                      elevation: 6.0,
+                                      constraints: const BoxConstraints.tightFor(
+                                        height: 40.0,
+                                        width: 40.0,
+                                      ),
+                                      fillColor: const Color(0xff4c4f5e),
+                                      shape: const CircleBorder(),
+                                      child: const Text('-'),
+                                      onPressed: () {
+                                        setState(() {
+                                          personData.minusWeight();
+                                        });
+                                      }),
+                                ],
+                              )
+                            ],
+                          ))),
+                  const SizedBox(
+                    width: 5,
+                  ),
                   Expanded(
                       flex: 1,
-                      child: card(nonActiveCardColor)),
+                      child: card(
+                          nonActiveCardColor,
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text('AGE'),
+                              Text('${personData.getAge()}'),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  RawMaterialButton(
+                                      elevation: 6.0,
+                                      fillColor: Color(0xff4c4f5e),
+                                      constraints: const BoxConstraints.tightFor(
+                                        height: 40.0,
+                                        width: 40.0,
+                                      ),
+                                      child: const Text(
+                                        '+',
+                                        style: TextStyle(),
+                                      ),
+                                      shape: const CircleBorder(),
+                                      onPressed: () {
+                                        setState(() {
+                                          personData.addAge();
+                                        });
+                                      }),
+                                  RawMaterialButton(
+                                      elevation: 6.0,
+                                      constraints: const BoxConstraints.tightFor(
+                                        height: 40.0,
+                                        width: 40.0,
+                                      ),
+                                      fillColor: const Color(0xff4c4f5e),
+                                      shape: const CircleBorder(),
+                                      child: const Text('-'),
+                                      onPressed: () {
+                                        setState(() {
+                                          personData.minusAge();
+                                        });
+                                      }),
+                                ],
+                              )
+                            ],
+                          ))),
                 ],
               ),
             ),
+            const SizedBox(
+              height: 5,
+            ),
             Container(
+              decoration: const BoxDecoration(color: Colors.blueAccent),
+              child: Center(
+                  child: TextButton(onPressed: () {
+                    personData.calcBMI();
+
+                  }, child: const Text('Calc your BMI', style: TextStyle(color: Colors.white)), )),
               height: 50,
             )
           ],
@@ -76,25 +199,19 @@ class _InputState extends State<Input> {
   }
 
   Widget genderCard(Gender gender) {
-
-    Color activeColor = personData.getGender() == gender? activeCardColor : nonActiveCardColor;
+    Color activeColor = personData.getGender() == gender ? activeCardColor : nonActiveCardColor;
     String genderText = gender == Gender.male ? 'MALE' : 'FEMALE';
     IconData genderIcon = gender == Gender.male ? Icons.male : Icons.female;
 
     return TextButton(
-        style: TextButton.styleFrom(
-            padding: EdgeInsets.zero
-        ),
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
         onPressed: () {
           setState(() {
             personData.setGender(gender);
           });
         },
         child: Container(
-          decoration: BoxDecoration(
-              color:  activeColor,
-              borderRadius: BorderRadius.circular(10.0)
-          ),
+          decoration: BoxDecoration(color: activeColor, borderRadius: BorderRadius.circular(10.0)),
           height: 150,
           width: double.infinity,
           child: Column(
@@ -105,23 +222,22 @@ class _InputState extends State<Input> {
                 color: Colors.white,
                 size: 50,
               ),
-              Text(genderText, style: const TextStyle(color: Colors.white),)
+              Text(
+                genderText,
+                style: const TextStyle(color: Colors.white),
+              )
             ],
           ),
         ));
   }
 
-  Widget card(Color color) {
+  Widget card(Color color, Widget columnWidget) {
     return TextButton(
-        style: TextButton.styleFrom(
-            padding: EdgeInsets.zero
-        ),
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
         onPressed: () {},
         child: Container(
-          decoration: BoxDecoration(
-              color:  color,
-              borderRadius: BorderRadius.circular(10.0)
-          ),
+          child: columnWidget,
+          decoration: BoxDecoration(color: color, borderRadius: BorderRadius.circular(10.0)),
           height: 150,
         ));
   }
