c29b4731f599198bc5bce3bd7a9cee8f3be20aca
qkrxodud00 <qkrxodud00@gmail.com>
2022-02-09 17:17:58 +0900

    *사람데이터객체 수정

--- lib/person_data.dart
+++ lib/person_data.dart
@@ -4,17 +4,60 @@ enum Gender{
 
 class PersonData {
   late Gender _gender;
-  late int _height;
+  late double _height;
   late int _weight;
   late int _age;
 
-  PersonData(this._gender, this._height, this._weight, this._age);
+  PersonData({required gender, required height, required weight, required age}) {
+    _gender = gender;
+    _height = height;
+    _weight = weight;
+    _age = age;
+  }
 
   Gender getGender() {
     return _gender;
   }
 
-  setGender(Gender gender) {
+  void setGender(Gender gender) {
     _gender = gender;
+  }
+
+  double getHeight() {
+    return _height;
+  }
+
+  void setHeight(double height) {
+    _height = height;
+  }
+
+  void addWeight() {
+    ++_weight;
+  }
+
+  void minusWeight() {
+    --_weight;
+  }
+
+  int getWeight() {
+    return _weight;
+  }
+
+  void addAge() {
+    ++_age;
+  }
+
+  void minusAge() {
+    --_age;
+  }
+
+  int getAge() {
+    return _age;
+  }
+
+  double calcBMI() {
+    double mHeight = _height/100;
+    print(_weight/(mHeight*mHeight));
+    return _weight/(mHeight*mHeight);
   }
 }
